f411eae02a673eb6555bd1d689875fc66bca8b48
aidenhsy <aidenhsy@hotmail.com>
2024-12-05 17:42:59 +0900

    Initial Commit

---
+++ Snack App/Snack_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snack_AppApp.swift
+//  Snack App
+//
+//  Created by Aiden Yang on 2024/12/5.
+//
+
+import SwiftUI
+
+@main
+struct Snack_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
